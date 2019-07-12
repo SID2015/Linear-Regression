e8f7f1e81aaf6b2f6f0e314710161a2771a43112
Shailendra Singh Rathore <SID2015@users.noreply.github.com>
2019-07-12 17:41:06 +0530

    Update ex1_multi.m

--- ex1_multi.m
+++ ex1_multi.m
@@ -79,7 +79,7 @@ fprintf(' %f \n', theta);
 fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
-% ====================== YOUR CODE HERE ======================
+% ====================== OUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % We should change this
